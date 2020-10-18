e16b94c41ac6818eca1b58c3d74814de780038cc
wadeepat <leaw.wadeepat@gmail.com>
2020-10-18 12:12:07 +0700

    add zombie

--- src/principal/statemachine/gamestate/Shop.java
+++ src/principal/statemachine/gamestate/Shop.java
@@ -13,58 +13,47 @@ import java.io.IOException;
 
 public class Shop implements GameState {
     private Font font;
-    private Sprite[] shopchar;
+    private Sprite[] shopchar; //array of character images
     private Sprite bg;
     private Sprite block;
-    private Font PixelMplus;
+
     public Rectangle backButton = new Rectangle(10,15,40,25);
-//    public Rectangle selectButton = new Rectangle(90, 180, 35, 15);
+
 public Rectangle selectButton[] = new Rectangle[15];
-    public boolean selected = false;
+
     public Shop(){
 
         shopchar = Game.animations.getShopchar();
         bg = Game.animations.getBgShop();
         block = Game.animations.getBlock();
-//        for(int i=0;i<15;i++){
-//            selectButton[i] = new Rectangle(90,180,40,25);
-//        }
-//        try{
-//            PixelMplus = Font.createFont(Font.TRUETYPE_FONT, new File("PixelMplus10-Regular.ttf"));
-//            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-//            ge.registerFont(PixelMplus);
-//        }catch (IOException | FontFormatException e){
-//            System.out.println("something Wrong");
-//        }
+
         font = new Font("Lucida Sans",Font.PLAIN,13);
     }
     @Override
     public void draw(Graphics2D g, long time) {
         g.setFont(font);
-        g.setColor(Color.white);
-        g.drawImage(bg.getImage(),0,0,null);
-        g.draw(backButton);
+        g.setColor(Color.white); //set font color
+        g.drawImage(bg.getImage(),0,0,null); //background image
+
+        g.draw(backButton); //draw back button
         g.drawString("Back", backButton.x+5, backButton.y+15);
 
         int i= 0;
-        for (int y=70 ;y<=390;y+=155){ //draw block
+        for (int y=70 ;y<=390;y+=155){ //draw shop window
             for(int x=60;x<=630;x+=140){
-                g.drawImage(block.getImage(),x,y,null);
-                g.drawImage(shopchar[i].getImage(),x+20,y+20,null);
-                selectButton[i] = new Rectangle(x+30,y+110,35,15);
-                g.drawString("select",x+30,y+120);
+                g.drawImage(block.getImage(),x,y,null); //draw block
+                g.drawImage(shopchar[i].getImage(),x+20,y+20,null); //draw  character
+                selectButton[i] = new Rectangle(x+30,y+110,35,15); //create select button
+                g.drawString("select",x+30,y+120); //draw string select
                 i++;
             }
         }
-
-
-
 
     }
 
     @Override
     public void tick(long time) {
-        if (MouseInput.leftClick){
+        if (MouseInput.leftClick){ //select character
             if (selectButton[0].contains(MouseInput.getPointer())){
                 System.out.println("selected 0");
                 Game.animations = new Images("conan");
@@ -76,6 +65,7 @@ public Rectangle selectButton[] = new Rectangle[15];
                 Game.animations = new Images("red");
             }else if (selectButton[3].contains(MouseInput.getPointer())){
                 System.out.println("selected 3");
+                Game.animations = new Images("zombie");
             }else if (selectButton[4].contains(MouseInput.getPointer())){
                 System.out.println("selected 4");
             }else if (selectButton[5].contains(MouseInput.getPointer())){
@@ -86,7 +76,7 @@ public Rectangle selectButton[] = new Rectangle[15];
                 System.out.println("selected 7");
             }
 
-            if(backButton.contains(MouseInput.getPointer())){
+            if(backButton.contains(MouseInput.getPointer())){ //if pressed back button go to menu
                 GameStatus.changeState(0);
             }
         }
